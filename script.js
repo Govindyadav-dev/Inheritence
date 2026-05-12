045657331286001d95fa9ee0e8cde18de294daa7
Govindyadav-dev <ygovind887430@gmail.com>
2026-05-12 09:21:58 +0530

    updated

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 //  from an existing class( parent se child inherit krta h properties and methods) "
 // Inheritence helps with code reusability
 
-
+// JavaScript is not truly class-based. It is a prototypal language. 
+// The class syntax introduced in ES6 is just syntactic sugar 
+// over the existing prototype-based inheritance. 
+// Under the hood, JavaScript still uses prototype chains to handle inheritance and method sharing."
 
 class Animal{
     alive= true;
@@ -47,3 +50,35 @@ rabbit.eat()
 fish.swim()
 fish.eat()
 fish.sleep()
+
+
+
+// JavaScript is not truly class-based. It is a prototypal language. 
+// The class syntax introduced in ES6 is just syntactic sugar 
+// over the existing prototype-based inheritance. 
+// Under the hood, JavaScript still uses prototype chains to handle inheritance and method sharing."
+
+
+//What actually happens behind the scenes?
+
+//When you write:
+
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log("Hi " + this.name);
+  }
+}
+
+//JavaScript internally converts it roughly to:
+
+function User(name) {
+  this.name = name;
+}
+
+User.prototype.sayHi = function () {
+  console.log("Hi " + this.name);
+};
